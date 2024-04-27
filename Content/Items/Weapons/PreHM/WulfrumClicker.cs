fb085da8efb921c20e43d01533957535d595f936
WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
2024-04-27 08:09:53 +0300

    Added a Wulfrum Clicker

---
+++ Content/Items/Weapons/PreHM/WulfrumClicker.cs
@@ -0,0 +1,59 @@
+﻿using CalamityMod.Items;
+using CalamityMod.Items.Materials;
+using ClickerClass;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+
+namespace CalamityClickers.Content.Items.Weapons.PreHM
+{
+    public class WulfrumClicker : ModdedClickerWeapon
+    {
+        public static string ClickerEffect { get; internal set; } = string.Empty;
+        public override float Radius => 1.5f;
+        public override Color RadiusColor => new Color(160, 237, 0);
+        public override void SetStaticDefaultsExtra()
+        {
+            ClickerEffect = ClickerSystem.RegisterClickEffect(Mod, "BasicAutoclicker", 1, RadiusColor, delegate (Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, int type, int damage, float knockBack)
+            {
+
+            }, true);
+        }
+        public override void SetDefaultsExtra()
+        {
+            AddEffect(Item, ClickerEffect);
+            SetDust(Item, 229);
+
+            Item.damage = 4;
+            Item.knockBack = 1f;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = CalamityGlobalItem.Rarity1BuyPrice;
+
+        }
+        public bool autoClicker = true;
+        public override void UpdateInventory(Player player)
+        {
+            if (player.HeldItem.type == Item.type && autoClicker)
+            {
+                ClickerCompat.SetAutoReuseEffect(player, 8);
+            }
+        }
+        public override bool CanRightClick()
+        {
+            return true;
+        }
+        public override void RightClick(Player player)
+        {
+            autoClicker = !autoClicker;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient<WulfrumMetalScrap>(8)
+                .AddIngredient<EnergyCore>()
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+    }
+}
