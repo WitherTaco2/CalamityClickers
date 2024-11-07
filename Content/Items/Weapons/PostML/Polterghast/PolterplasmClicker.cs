2d309b8e0b4e1357ea930f8c440c91fd128d3df7
WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
2024-11-07 12:40:11 +0300

    Polterplasm Clicker Resprite

--- Content/Items/Weapons/PostML/Polterghast/PolterplasmClicker.cs
+++ Content/Items/Weapons/PostML/Polterghast/PolterplasmClicker.cs
@@ -2,7 +2,11 @@
 using CalamityMod.Items.Materials;
 using ClickerClass.Items.Weapons.Clickers;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.GameContent;
 using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace CalamityClickers.Content.Items.Weapons.PostML.Polterghast
 {
@@ -20,6 +24,36 @@ namespace CalamityClickers.Content.Items.Weapons.PostML.Polterghast
             Item.knockBack = 1f;
             Item.rare = ItemRarityID.Red;
             Item.value = CalamityGlobalItem.RarityRedBuyPrice;
+        }
+        private void DrawingAnimation(SpriteBatch spriteBatch, Vector2 baseDrawPosition, Rectangle frame, float baseScale, Color drawColor)
+        {
+            //if (Item.velocity.X != 0f)
+            //    return;
+            Texture2D t = ModContent.Request<Texture2D>(Texture + "/" + ((int)Main.GlobalTimeWrappedHourly % 5).ToString()).Value;
+
+            spriteBatch.Draw(t, baseDrawPosition, frame, drawColor, 0f, Vector2.Zero, baseScale, SpriteEffects.None, 0f);
+
+        }
+        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+        {
+            if (!CalamityClickersConfig.Instance.LegecyClickerTextures)
+            {
+                Rectangle frame = TextureAssets.Item[Item.type].Value.Frame();
+                DrawingAnimation(spriteBatch, Item.position - Main.screenPosition, frame, scale, lightColor);
+                return true;
+            }
+            return base.PreDrawInWorld(spriteBatch, lightColor, alphaColor, ref rotation, ref scale, whoAmI);
+        }
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            if (!CalamityClickersConfig.Instance.LegecyClickerTextures)
+            {
+                //Rectangle frame = TextureAssets.Item[Item.type].Value.Frame();
+                Item.velocity.X = 0f;
+                DrawingAnimation(spriteBatch, Item.position - frame.Size() * 0.25f, frame, scale, drawColor);
+                return true;
+            }
+            return PreDrawInInventory(spriteBatch, position, frame, drawColor, itemColor, origin, scale);
         }
         public override void AddRecipes()
         {
